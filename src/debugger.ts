4aae02b1bbc9981d916b919250571d9258a885ee
Nicolas Berthier <nicolas.berthier@ocamlpro.com>
2026-02-11 08:49:20 +0100

    Fix loading of modules via `cobcrun`, discarding `-m` argument

--- src/debugger.ts
+++ src/debugger.ts
@@ -492,7 +492,7 @@ export class DebuggerVariable {
 export interface IDebugger {
     load(cwd: string, target: string, targetargs: string, group: string[], gdbtty: boolean): Thenable<any>;
 
-    attach(cwd: string, target: string, targetargs: string, group: string[]): Thenable<any>;
+    attach(cwd: string, target: string, group: string[]): Thenable<any>;
 
     start(pid?: string): Thenable<boolean>;
 
